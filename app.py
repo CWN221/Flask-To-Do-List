ed888b127d46398561346e819f92e3d1d385faba
CWN221 <159309949+CWN221@users.noreply.github.com>
2026-02-16 23:58:37 +0300

    Added login & registration page

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, session, redirect, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user, UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, EqualTo, Length, Email
 from datetime import datetime
 from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "Login"
 
 # Session
 app.config["SESSION_PERMANENT"] = False
@@ -18,7 +28,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 # Database Model
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), nullable=False)
@@ -37,14 +47,78 @@ with app.app_context():
     db.create_all()
 
 
-# Routes
+# Registration & Login Forms
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(), Length(min=2, max=30)])
+    email = StringField("Email", validators=[InputRequired(), Email()])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=6)])
+    confirm_password = PasswordField("Confirm Password", validators=[InputRequired(), EqualTo("password")])
+    submit = SubmitField("Register")
+
+class LoginForm(FlaskForm):
+    email = StringField("email", validators=[InputRequired(), Email()])
+    password = StringField("password", validators=[InputRequired()])
+    submit = SubmitField("Login")
+
+# User authentication
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route("/register", methods=["POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        new_user = User(username = form.username.data, email = form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("User registered successfully.", category="success")
+        return redirect(url_for("login"))
+    
+    return render_template("register.html", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash("Login successful.", category="success")
+            return redirect(url_for("index"))
+        else:
+            flash("Login failed. Check Email & password.", category="error")
+    
+    return render_template("login.html", form=form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("User logged out.", category="success")
+    return redirect(url_for("login"))
+
+
+# Routes for handling tasks
 @app.route("/")
+@login_required
 def index():
     tasks = Task.query.order_by(Task.created_at.desc()).all()
     return render_template("index.html", tasks=tasks)
 
 
 @app.route("/", methods=['POST'])
+@login_required
 def add_task():
     title = request.form.get("title")
     priority = request.form.get("priority")
@@ -68,6 +142,7 @@ def add_task():
 
 
 @app.route("/delete/<int:task_id>", methods=['GET'])
+@login_required
 def delete_task(task_id):
     task = Task.query.get_or_404(task_id)
     db.session.delete(task)
@@ -77,6 +152,7 @@ def delete_task(task_id):
 
 
 @app.route("/complete/<int:task_id>", methods=['GET'])
+@login_required
 def complete_task(task_id):
     task = Task.query.get_or_404(task_id)
     task.done = not task.done
@@ -86,6 +162,7 @@ def complete_task(task_id):
 
 
 @app.route("/edit/<int:task_id>", methods=["GET", "POST"])
+@login_required
 def edit_task(task_id):
     task = Task.query.get_or_404(task_id)
 
@@ -99,3 +176,4 @@ def edit_task(task_id):
         flash("Task Updated successfully", category="success")
         return redirect(url_for("index"))
     return render_template("edit.html", task=task)
+
